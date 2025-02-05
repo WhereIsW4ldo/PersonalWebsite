4d03625bd7697e48fa79bcdaa7d04e7cb6d28505
waldo wautelet <waldowautelet4@gmail.com>
2025-02-05 14:53:19 +0100

    feat: add logging to loki

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -1,6 +1,24 @@
 using Backend.Api.Middleware;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+
+var otlpEndpointUrl = builder.Configuration["OTLP_ENDPOINT_URL"];
+
+if (!string.IsNullOrEmpty(otlpEndpointUrl))
+{
+    builder.Logging.AddOpenTelemetry(x =>
+    {
+        x.AddOtlpExporter(a =>
+        {
+            a.Endpoint = new Uri(otlpEndpointUrl);
+            a.Protocol = OtlpExportProtocol.HttpProtobuf;
+        });
+    });
+}
 
 // Add services to the container
 builder.Services.AddControllers(o =>
