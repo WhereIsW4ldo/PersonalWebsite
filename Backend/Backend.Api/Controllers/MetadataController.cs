c66d3786433de30af1ebae2ff77d0c70ef154848
waldo wautelet <waldowautelet4@gmail.com>
2025-02-07 10:28:19 +0100

    feat: versioning of backend package

--- Backend/Backend.Api/Controllers/MetadataController.cs
+++ Backend/Backend.Api/Controllers/MetadataController.cs
@@ -1,6 +1,38 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using ILogger = Serilog.ILogger;
+
 namespace Backend.Api.Controllers;
 
-public class MetadataController
+[ApiController]
+public class MetadataController : ControllerBase
 {
-    
+    private readonly ILogger _logger;
+
+    public MetadataController(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("version")]
+    public async Task<IActionResult> GetVersion()
+    {
+        try
+        {
+            var informationalVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+            
+            if (informationalVersion is null)
+            {
+                throw new Exception("Version is null");
+            }
+            
+            return Ok(informationalVersion);
+        }
+        catch (Exception e)
+        {
+            _logger.Error(e, "Something went wrong while getting version");
+            return BadRequest(e.Message);
+        }
+    }
 }
